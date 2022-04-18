3fbd816d3405d7a164ba4e007dd3a0522a5dc961
Zina Kamel <zena.kamel100@gmail.com>
2022-04-18 13:13:28 +0300

    updating the table UI

--- src/components/Systems/index.js
+++ src/components/Systems/index.js
@@ -21,14 +21,14 @@ const Systems = () => {
 
     useEffect(()=>{
         getSystems();
-        setRows(systems.map((system) => ({id: system.roverName, roverName: system.roverName, cameraName: system.cameraName, imageQuality: system.imageQuality, videoCollection: system.videoCollection})));
+        setRows(systems.map((system) => ({id: system.roverName, roverName: system.roverName, cameraName: system.cameraName, imageQuality: system.imageQuality, numberOfVideos: system.numberOfVideos})));
     },[systems])
     
     const columns = [
-        { field: 'roverName', headerName: 'Rover Name', width: 130 },
-        { field: 'cameraName', headerName: 'Camera Name', width: 130 },
-        { field: 'imageQuality', headerName: 'Image Quality', width: 130 },
-        { field: 'videoCollection', headerName: 'Video Collection', width: 130 },
+        { field: 'roverName', headerName: 'Rover Name', width: 310 },
+        { field: 'cameraName', headerName: 'Camera Name', width: 310 },
+        { field: 'imageQuality', headerName: 'Image Quality', width: 310 },
+        { field: 'numberOfVideos', headerName: 'Number of Videos', width: 310 },
     ];
 
     let navigate = useNavigate();
@@ -43,7 +43,7 @@ const Systems = () => {
                 <button>Classify</button>
             </nav>
 
-            <div style={{ height: 400, width: '100%', marginTop: 16}}>
+            <div style={{ height: 450, width: '100%', marginTop: 16, color: 'white'}}>
             <DataGrid style={{color: 'white'}}
                 rows={rows}
                 columns={columns}
